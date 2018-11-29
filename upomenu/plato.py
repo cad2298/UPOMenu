de41075164c95268da588db83473b5b699a7f3dd
Sergio Luzuriaga <39329863+MrMerilio@users.noreply.github.com>
2018-11-29 20:02:53 +0100

    Update plato.py

--- upomenu/plato.py
+++ upomenu/plato.py
@@ -36,6 +36,6 @@ class plato(osv.Model):
                                      ], 'Tipo de plato'),
             'precio':fields.float('Precio', digits=(6, 2)),
             'ingrediente_ids':fields.many2many('ingrediente','plato_ingrediente_rel','plato_id','ingrediente_id','Ingredientes'),
-            #'platosmenu_ids':fields.one2many('platosmenu','plato_id','Platomenu'),
+            'platosmenu_ids':fields.one2many('platosmenu','plato_id','Platomenu'),
         }
             
